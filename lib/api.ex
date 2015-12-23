fc13b493868ef04e2a04fcd97168d44b515c59c4
Tim Herd <rz@eqdw.net>
2015-12-22 22:41:04 -0600

    api stuff

--- lib/api.ex
+++ lib/api.ex
@@ -1,14 +1,55 @@
-#defmodule Stockfighter.Response do
+#defmodule Stockranger.Response do
   #defstruct
 #end
 
-defmodule Stockfighter.Api do
-  @heartbeat_url "https://api.stockfighter.io/ob/api/heartbeat"
+defmodule Stockranger.Api do
+
+  @api_base "https://api.stockfighter.io/ob/api"
 
   def heartbeat do
-    case HTTPoison.get(@heartbeat_url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        Poison.decode!(body)["ok"]
+    %{data: data} = get("/heartbeat")
+    data["ok"]
+  end
+
+  def heartbeat(venue) do
+    %{data: data} = get("/venues/#{venue}/heartbeat")
+    data["ok"]
+  end
+
+  def stocks(venue) do
+    %{data: data} = get("/venues/#{venue}/stocks")
+
+    case data do
+      %{"ok" => true, "symbols" => symbols} ->
+        Enum.map(symbols, fn(stock) -> Stockranger.Stock.create(stock) end)
+      %{"ok" => false, "error" => error} ->
+        error
+    end
+  end
+
+  def order_book(venue, stock) do
+    %{data: data} = get("/venues/#{venue}/stocks/#{stock}")
+
+    case data do
+      %{"ok" => true} ->
+        Stockranger.OrderBook.create(data)
+      %{"ok" => false, "error" => error} ->
+        error
+    end
+  end
+
+  def post(url) do
+  end
+
+  def get(url) do
+    HTTPoison.get("#{@api_base}#{url}")
+    |> process_response
+  end
+
+  defp process_response(response) do
+    case response do
+      {:ok, %HTTPoison.Response{status_code: status, body: body}} ->
+        %{status: status, data: Poison.decode!(body)}
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.inspect reason
     end
